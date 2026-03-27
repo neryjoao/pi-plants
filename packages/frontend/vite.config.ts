abbc0d1a4541c277216497f8afe747a27eb645f4
neryjoao <39858298+neryjoao@users.noreply.github.com>
2026-03-27 10:40:49 +0100

    - added favicon

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    allowedHosts: ['plants.local'],
+    allowedHosts: ['plants.local', 'localhost'],
     proxy: {
       '/plantsDetails': 'http://localhost:3001',
       '/name': 'http://localhost:3001',
